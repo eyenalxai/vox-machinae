7f68bcfb8cfb72e8ab40f2bf321317b074df4488
Sax Dhiut <mail@iyxeyl.me>
2023-02-01 22:51:31 +0400

    fix: eh #2

--- app/util/lifecycle/lifecycle_functions.py
+++ app/util/lifecycle/lifecycle_functions.py
@@ -7,10 +7,9 @@ from app.util.settings.shared import shared_settings
 async def on_startup(bot: Bot) -> None:
     if shared_settings.poll_type == "WEBHOOK":
         webhook_url = shared_settings.webhook_url
-
         await bot.set_webhook(webhook_url)
         logger.info("Webhook set to: %s", webhook_url)
 
 
-async def on_shutdown(bot: Bot) -> None:
+async def on_shutdown() -> None:
     logger.info("Shutting down...")
