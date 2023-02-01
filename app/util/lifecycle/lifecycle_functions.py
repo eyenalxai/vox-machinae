1550cfa410dcbfd6a97b5ac513f6c7300d5bfcb8
Sax Dhiut <mail@iyxeyl.me>
2023-02-01 22:48:11 +0400

    fix: eh

--- app/util/lifecycle/lifecycle_functions.py
+++ app/util/lifecycle/lifecycle_functions.py
@@ -1,5 +1,3 @@
-from asyncio import sleep
-
 from aiogram import Bot
 
 from app.config.log import logger
@@ -10,15 +8,9 @@ async def on_startup(bot: Bot) -> None:
     if shared_settings.poll_type == "WEBHOOK":
         webhook_url = shared_settings.webhook_url
 
-        logger.info("Sleeping for 60 seconds...")
-        await sleep(60)
-
         await bot.set_webhook(webhook_url)
         logger.info("Webhook set to: %s", webhook_url)
 
 
 async def on_shutdown(bot: Bot) -> None:
     logger.info("Shutting down...")
-    if shared_settings.poll_type == "WEBHOOK":
-        await bot.delete_webhook()
-        logger.info("Webhook removed")
