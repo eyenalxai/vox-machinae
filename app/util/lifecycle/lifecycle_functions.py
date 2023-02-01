8e4c15a1e2afcb6876dd95e6e5a21a704b2cb0c7
Sax Dhiut <mail@iyxeyl.me>
2023-02-01 22:47:06 +0400

    feat: sleep for 60 seconds

--- app/util/lifecycle/lifecycle_functions.py
+++ app/util/lifecycle/lifecycle_functions.py
@@ -1,3 +1,5 @@
+from asyncio import sleep
+
 from aiogram import Bot
 
 from app.config.log import logger
@@ -7,6 +9,9 @@ from app.util.settings.shared import shared_settings
 async def on_startup(bot: Bot) -> None:
     if shared_settings.poll_type == "WEBHOOK":
         webhook_url = shared_settings.webhook_url
+
+        logger.info("Sleeping for 60 seconds...")
+        await sleep(60)
 
         await bot.set_webhook(webhook_url)
         logger.info("Webhook set to: %s", webhook_url)
